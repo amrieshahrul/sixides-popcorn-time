9ec013c7caa8b06b85e56dbe8488a724d8a970ec
amrieshahrul <amrieshahrulceo@gmail.com>
2024-02-13 18:22:41 +0800

    Build: Updated tailwind config and global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+				'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'primary-grey': '#777777',
+				'primary-blue': '#17b7da',
+				'divider-grey': '#f1f1f1',
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+				// TMDB color preset
+				'tmdbLightGrey': '#e3e3e3',
+				'tmdbDarkBlue': '#032541',
+				'tmdbLightBlue': '#01b4e4',
+				'tmdbLighterGreen': '#c0fed0',
+				'tmdbLightGreen': '#1ed5a9',
+				'tmdbLogoGreen': '#90cea1',
+				'tmdbLogoOrange': '#fdc070',
+				'tmdbLogoRed': '#d93b63',
+			},
+		},
+	},
+	plugins: [],
 };
-export default config;
