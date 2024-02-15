dae23e30a62d5c1b13ef0c387d6104d872a355c1
amrieshahrul <amrieshahrulceo@gmail.com>
2024-02-15 14:34:01 +0800

    Dev: Added mocks data

---
+++ src/mocks/sortByList.js
@@ -0,0 +1,10 @@
+export const sortByList = [
+	{ label: 'Title (A-Z)', value: 'title.asc' },
+	{ label: 'Title (Z-A)', value: 'title.desc' },
+	{ label: 'Release Ascending', value: 'primary_release_date.asc' },
+	{ label: 'Release Descending', value: 'primary_release_date.desc' },
+	{ label: 'Popularity Ascending', value: 'popularity.asc' },
+	{ label: 'Popularity Descending', value: 'popularity.desc' },
+	{ label: 'Rating Ascending', value: 'vote_average.asc' },
+	{ label: 'Rating Descending', value: 'vote_average.desc' },
+];
