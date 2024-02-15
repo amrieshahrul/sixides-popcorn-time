4601b8a7233df77c4b887cea2d5e5803f31650c3
amrieshahrul <amrieshahrulceo@gmail.com>
2024-02-15 14:41:40 +0800

    Dev: Defined time helpers.

---
+++ src/helpers/time.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+
+export const todayMinusOne = (): string => {
+	return dayjs().subtract(1, 'day').format('YYYY-MM-DD');
+};
+
+export const monthsFromToday = (): string => {
+	return dayjs().subtract(43, 'days').format('YYYY-MM-DD');
+};
+
